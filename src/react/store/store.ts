68b3f55b9681cd83018bdfa53d28ffcdac44e248
benedicti0n <lbandopadhyay2@gmail.com>
2025-08-27 11:04:33 +0530

    tailwind + shadcn + zustand

---
+++ src/react/store/store.ts
@@ -0,0 +1,12 @@
+// store.ts
+import { create } from "zustand";
+
+type CounterState = {
+    count: number;
+    increment: () => void;
+};
+
+export const useCounterStore = create<CounterState>((set) => ({
+    count: 0,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+}));
